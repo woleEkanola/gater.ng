c3d08e5ad3a0e8c32c8cc7f9ad942b50a17b1c12
woleekanola <woleekanola@gmail.com>
2026-06-04 10:04:29 +0100

    chore: remove temp debug file

--- check-users.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-prisma.user.findMany({
-  where: { role: { in: ["ADMIN", "SUPERADMIN"] } },
-  select: { email: true, name: true, role: true },
-}).then(r => { console.log(JSON.stringify(r, null, 2)); prisma.$disconnect(); });
